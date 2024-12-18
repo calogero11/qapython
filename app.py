0d1eb26ba722c6229048255317835c70d097b5c4
EC2 Default User <ec2-user@ip-172-31-35-205.eu-west-1.compute.internal>
2024-12-18 15:45:01 +0000

    made it work on ec2

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def update(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
